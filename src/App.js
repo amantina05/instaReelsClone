eb9f35cac24fd3750e659322ae598f7269cafacc
Amantina Sosa <amantinasosa@gmail.com>
2020-09-03 12:56:08 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app_top">
+        <img
+          className="app_logo"
+          src="https://www.edigitalagency.com.au/wp-content/uploads/instagram-logo-svg-vector-for-print.svg"
+          alt=""
+        />
+        <h1>Reels</h1>
+      </div>
+      <div className="app_videos">
+        {/* container of app videos (scrollable) */}
+        {/* Video */}
+        {/* Video */}
+        {/* Video */}
+        {/* Video */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
